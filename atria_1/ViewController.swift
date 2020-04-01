c85b52a45b91cb649956d4997da8ddaa14f8a764
Jeffrey Chen <j.chen14641@gmail.com>
2020-04-01 14:35:06 -0700

    Initial Commit

---
+++ atria_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  atria_1
+//
+//  Created by Jeffrey Chen on 4/1/20.
+//  Copyright © 2020 Jeffrey Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
